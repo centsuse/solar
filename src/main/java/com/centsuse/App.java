46540b8e15725f7c91c0496e4e8f9cd8dd489e12
centsuse <903591127@qq.com>
2018-10-31 22:11:57 +0800

    init

---
+++ src/main/java/com/centsuse/App.java
@@ -0,0 +1,13 @@
+package com.centsuse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
